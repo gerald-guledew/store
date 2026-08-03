2ce1084f0fb3f1de1fb4d7bad18c1d57b260103d
Gerald Guledew <gerald.guledew@gmail.com>
2026-08-04 11:03:43 +1200

    Added flyway maven plugin in pom.xml and setup environment variables for user and password.

--- src/main/java/com/gguledew/store/StoreApplication.java
+++ src/main/java/com/gguledew/store/StoreApplication.java
@@ -2,6 +2,7 @@ package com.gguledew.store;
 
 import com.gguledew.store.domain.User;
 import com.gguledew.store.service.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -16,6 +17,12 @@ public class StoreApplication {
 //        context.close();
 
         ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+        String user = System.getenv("DB_USER");
+        String password = System.getenv("DB_PASSWORD");
+
+        System.out.println("Environment Variable DB_USER: "+ user);
+        System.out.println("Environment Variable DB_PASSWORD: "+ password);
+        System.out.println("Environment Variable db_user: "+ context.getEnvironment().getProperty("db_user"));
 //        UserService userService = context.getBean(UserService.class);
 //        userService.registerUser(new User(1L, "gerald.g@gmail.com","1234","Gerald"));
 //        userService.registerUser(new User(1L, "gerald.g@gmail.com","1234","Gerald"));
