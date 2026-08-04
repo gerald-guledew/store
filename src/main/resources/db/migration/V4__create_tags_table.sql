f48b1ab4673eabd010e8fd7617c01bd9832d92a1
Gerald Guledew <gerald.guledew@gmail.com>
2026-08-04 14:39:23 +1200

    Added migration scripts for creating tags and user_tags table.

---
+++ src/main/resources/db/migration/V4__create_tags_table.sql
@@ -0,0 +1,6 @@
+create table tags
+(
+    id   bigint       not null
+        primary key,
+    name varchar(255) not null
+);
---
+++ src/main/resources/db/migration/V5__create_user_tags_table.sql
@@ -0,0 +1,11 @@
+create table user_tags
+(
+    user_id bigint not null,
+    tag_id  bigint not null,
+    constraint user_tags_pk
+        primary key (user_id, tag_id),
+    constraint user_tags_tags_id_fk
+        foreign key (tag_id) references tags (id),
+    constraint user_tags_users_id_fk
+        foreign key (user_id) references users (id)
+);
