ed65681b70faaf77b04b08fb54cd197d38628afd
Gerald Guledew <gerald.guledew@gmail.com>
2026-08-22 23:43:00 +1200

    Adding method signature for different method to extract products

--- src/main/java/com/gguledew/store/repository/ProductRepository.java
+++ src/main/java/com/gguledew/store/repository/ProductRepository.java
@@ -1,7 +1,18 @@
 package com.gguledew.store.repository;
 
 import com.gguledew.store.domain.Product;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 public interface ProductRepository extends CrudRepository<Product, Long> {
+    List<Product> findByName(String name);
+    List<Product> findTop5ByNameOrderByPriceDesc(String name);
+    List<Product> findByPriceBetweenOrderByNameAsc(BigDecimal minPrice, BigDecimal maxPrice);
+    @Query(value = "select * from products p where p.price between :min and :max order by p.name", nativeQuery = true)
+    List<Product> findByProduct(@Param("min") BigDecimal min, @Param("max") BigDecimal max);
+
 }
