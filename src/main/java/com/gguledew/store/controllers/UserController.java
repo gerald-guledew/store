32f8e3a2c8704f4a340765658a76a810674bc7df
Gerald Guledew <gerald.guledew@gmail.com>
2026-09-23 16:10:01 +1200

    Added a DTO class ChangePasswordRequest. Added a DTO class UpdateUserRequest. Added updateUser method in UserController class. Added deleteUser method in UserController class. Added changePassword method in UserController class. Added updateUser method in UserMapper interface.

--- src/main/java/com/gguledew/store/controllers/UserController.java
+++ src/main/java/com/gguledew/store/controllers/UserController.java
@@ -1,6 +1,8 @@
 package com.gguledew.store.controllers;
 
+import com.gguledew.store.dtos.ChangePasswordRequest;
 import com.gguledew.store.dtos.RegisterUserRequest;
+import com.gguledew.store.dtos.UpdateUserRequest;
 import com.gguledew.store.dtos.UserDto;
 import com.gguledew.store.entities.User;
 import com.gguledew.store.mappers.UserMapper;
@@ -8,6 +10,7 @@ import com.gguledew.store.repositories.UserRepository;
 import com.gguledew.store.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -52,5 +55,47 @@ public class UserController {
         var uri = uriBuilder.path("/users/{id}").buildAndExpand(userDto.getId()).toUri();
 
         return ResponseEntity.created(uri).body(userDto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDto> updateUser(
+            @PathVariable(name="id") Long id,
+            @RequestBody UpdateUserRequest updateUser) {
+        var user = userRepository.findById(id).orElse(null);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        userMapper.updateUser(updateUser, user);
+        userRepository.save(user);
+        return ResponseEntity.ok(userMapper.toDto(user));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable(name="id") Long id) {
+        var user = userRepository.findById(id).orElse(null);
+        if(user == null) {
+            return ResponseEntity.notFound().build();
+        }
+        userRepository.delete(user);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping ("/{id}/change-password")
+    public ResponseEntity<Void> changePassword(
+            @PathVariable(name="id") Long id,
+            @RequestBody ChangePasswordRequest request) {
+        var user = userRepository.findById(id).orElse(null);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        if (!user.getPassword().equals(request.getOldPassword())) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+
+        user.setPassword(request.getNewPassword());
+        userRepository.save(user);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/gguledew/store/mappers/UserMapper.java
+++ src/main/java/com/gguledew/store/mappers/UserMapper.java
@@ -1,12 +1,15 @@
 package com.gguledew.store.mappers;
 
 import com.gguledew.store.dtos.RegisterUserRequest;
+import com.gguledew.store.dtos.UpdateUserRequest;
 import com.gguledew.store.dtos.UserDto;
 import com.gguledew.store.entities.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     UserDto toDto(User user);
     User toEntity(RegisterUserRequest registerUserRequest);
+    void updateUser(UpdateUserRequest updateUser, @MappingTarget User user);
 }
