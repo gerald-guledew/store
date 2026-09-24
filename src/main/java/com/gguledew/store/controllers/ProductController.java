b301b01b2421b0d9217d120f7dc4c283debfd6f2
Gerald Guledew <gerald.guledew@gmail.com>
2026-09-24 16:19:08 +1200

    Changed parent repository of CategoryRepository from CrudRepository to JpaRepository. This will allow us to use .getReferenceById(). Created createProduct() and updateProduct() methods in ProductController. Added toEntity() and updateProduct() methods in ProductMapper interface.

--- src/main/java/com/gguledew/store/controllers/ProductController.java
+++ src/main/java/com/gguledew/store/controllers/ProductController.java
@@ -3,10 +3,12 @@ package com.gguledew.store.controllers;
 import com.gguledew.store.dtos.ProductDto;
 import com.gguledew.store.entities.Product;
 import com.gguledew.store.mappers.ProductMapper;
+import com.gguledew.store.repositories.CategoryRepository;
 import com.gguledew.store.repositories.ProductRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -16,6 +18,7 @@ import java.util.List;
 public class ProductController {
     private ProductRepository productRepository;
     private ProductMapper productMapper;
+    private final CategoryRepository categoryRepository;
 
     @GetMapping
     public List<ProductDto> getProducts(@RequestParam(required = false, name = "categoryId") Byte categoryId) {
@@ -37,4 +40,36 @@ public class ProductController {
         }
         return ResponseEntity.notFound().build();
     }
+
+    @PostMapping
+    public ResponseEntity<ProductDto> createProduct(
+            @RequestBody ProductDto request,
+            UriComponentsBuilder uriBuilder) {
+        var product = productMapper.toEntity(request);
+        var category = categoryRepository.getReferenceById(request.getCategoryId());
+        product.setCategory(category);
+        productRepository.save(product);
+        var productDto = productMapper.toDto(product);
+        var uri = uriBuilder.path("/products/{id}").buildAndExpand(productDto.getId()).toUri();
+        return ResponseEntity.created(uri).body(productDto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductDto> updateProduct(
+            @PathVariable Long id,
+            @RequestBody ProductDto request){
+        var product = productRepository.findById(id).orElse(null);
+        if (product == null) {
+            return ResponseEntity.notFound().build();
+        }
+        productMapper.updateProduct(request, product);
+        if (request.getCategoryId() != null) {
+            var category = categoryRepository.getReferenceById(request.getCategoryId());
+            product.setCategory(category);
+        }
+        productRepository.save(product);
+        return ResponseEntity.ok(productMapper.toDto(product));
+    }
+
+
 }
--- src/main/java/com/gguledew/store/mappers/ProductMapper.java
+++ src/main/java/com/gguledew/store/mappers/ProductMapper.java
@@ -2,11 +2,16 @@ package com.gguledew.store.mappers;
 
 import com.gguledew.store.dtos.ProductDto;
 import com.gguledew.store.entities.Product;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
     @Mapping(source = "category.id", target = "categoryId")
     ProductDto toDto(Product product);
+    @Mapping(target="category", ignore = true)
+    Product toEntity(ProductDto request);
+    @Mapping(target="id", ignore=true)
+    @Mapping(target="category", ignore=true)
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateProduct(ProductDto request, @MappingTarget Product product);
 }
